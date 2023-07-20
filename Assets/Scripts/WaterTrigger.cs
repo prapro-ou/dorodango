00316ea2e7f477a96d25676997158d350e20fdc6
miukawahara <miu140613@gmial.com>
2023-07-20 18:14:36 +0900

    水の量を監視するゲージを作成しました

--- Assets/Scripts/WaterTrigger.cs
+++ Assets/Scripts/WaterTrigger.cs
@@ -7,11 +7,17 @@ public class WaterTrigger : MonoBehaviour
     public WaterPouring waterPouringScript;  // WaterPouringスクリプトの参照
 
     public bool flag = false;   // マウスが押されたかどうかのフラグ
+    private bool audioFlag = false; // Audioが再生されているかどうかのフラグ
+
+    // 水を流す効果音用
+    public AudioClip sound;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Componentを取得
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,8 +27,20 @@ public class WaterTrigger : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             flag = true;
+            if (audioFlag == false)
+            {
+                Invoke("WaterSound", 0.5f);
+                audioFlag = true;
+            }
         } else {
             flag = false;
+            audioFlag = false;
+            audioSource.Stop();
         }
+    }
+
+    void WaterSound()
+    {
+        audioSource.PlayOneShot(sound);    //音(sound)を鳴らす
     }
 }
