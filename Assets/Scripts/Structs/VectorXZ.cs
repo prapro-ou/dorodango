490b2475259e56f26bcccd1a00970d738aca5a0b
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2023-08-17 13:56:35 +0900

    RemoveGarbage シーンでの GameObject の生成を減らした
    Instanciate するかわりに position の変更にした
    それに合わせてクラス名を変更

---
+++ Assets/Scripts/Structs/VectorXZ.cs
@@ -0,0 +1,13 @@
+using System;
+
+// ReSharper disable once CheckNamespace
+// ReSharper disable once IdentifierTypo
+namespace Dorodango.Structs
+{
+    [Serializable]
+    public struct VectorXZ
+    {
+        public float x;
+        public float z;
+    }
+}
