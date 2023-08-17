ceddc5b2bdd9de6a1e7d27f071adf3de3b4d5b73
kinko173 <85325720+kinko173@users.noreply.github.com>
2023-08-17 17:20:01 +0900

    ゴミを取り除くシーンを追加 (#33)
    Close #22 
    
    ごみを拾い捨てる機能を追加しました．画面外近くにゴミを持っていくと消えます．
    マウスクリックで持ち上げホールドで移動します．

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private const float DistanceOfLift = 1.5f;
+    private const float MaxX = 9.0f; // カメラ座標xの最大値
+    private const float MaxZ = 4.5f; // カメラ座標zの最大値
+    [SerializeField] private AudioSource audioSource;
+    private new Collider collider;
+    private float defaultY;
+    private Camera mainCamera;
+    private bool pickedUp = false;
+
+    private void Start()
+    {
+        collider = GetComponent<Collider>();
+        mainCamera = Camera.main;
+        defaultY = transform.position.y;
+    }
+
+    private void Update()
+    {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        // クリックした瞬間にオブジェクトに当たっていれば持ち上げる
+        if (Input.GetMouseButtonDown(0) && DoesHitMe(ray))
+        {
+            pickedUp = true;
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+
+        // マウスホールドをやめたとき
+        if (Input.GetMouseButtonUp(0) && pickedUp)
+        {
+            pickedUp = false;
+            var newPosition = transform.position;
+            newPosition.y = defaultY;
+            transform.position = newPosition;
+            if (Mathf.Abs(transform.position.x) >= MaxX || Mathf.Abs(transform.position.z) >= MaxZ)
+                Destroy(gameObject); // 画面外にあれば破壊
+        }
+
+        // 持ち上げていてかつマウスをホールドしている間オブジェクトを移動
+        if (Input.GetMouseButton(0) && pickedUp)
+        {
+            RaycastHit hit;
+            if (!Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity)) return;
+            var newPosition = hit.point;
+            newPosition.y = defaultY + DistanceOfLift;
+            transform.position = newPosition;
+        }
+    }
+
+
+    private bool DoesHitMe(Ray ray)
+    {
+        RaycastHit hit;
+        return Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity) &&
+               hit.collider == collider;
+    }
+}
---
+++ Assets/Scripts/RandomizeXZPositionOfChildren.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Dorodango.Structs;
+using UnityEngine;
+
+public class RandomizeXZPositionOfChildren : MonoBehaviour
+{
+    [SerializeField] private VectorXZ minPosition;
+    [SerializeField] private VectorXZ maxPosition;
+
+    private void Start()
+    {
+        // Transform は IEnumerable を実装しており，直接の子オブジェクトの Transform が取得できる
+        // cf: https://docs.unity3d.com/2022.3/Documentation/ScriptReference/Transform.html
+        var children = transform.OfType<Transform>();
+        foreach (var child in children) RandomizeXZPosition(child);
+    }
+
+    private void RandomizeXZPosition(Transform t)
+    {
+        var newX = Random.Range(minPosition.x, maxPosition.x);
+        var newZ = Random.Range(minPosition.z, maxPosition.z);
+        t.position = new Vector3(newX, t.position.y, newZ);
+    }
+}
