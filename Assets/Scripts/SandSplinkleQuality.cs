279fd613d2a4c22caa22131364348a12a8e69853
kinko173 <85325720+kinko173@users.noreply.github.com>
2023-08-18 23:15:16 +0900

    砂を掛ける工程を作成
    手を左クリック長押しで操作し，泥団子に砂を掛ける．
    砂を掛けるとゲージが上昇する．

---
+++ Assets/Scripts/SandSplinkleQuality.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SandSplinkleSlider : MonoBehaviour
+{
+
+
+    private const float gaugeIncreaseAmount = 0.1f; // ゲージが増加する量
+    private const float maxGaugeValue = 100f; // ゲージの最大値
+    public Slider gaugeSlider; // ゲージのSlider
+
+    private void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("dorodango"))
+        {
+            Debug.Log(gaugeSlider.value);
+            // ゲージを増加させる
+            gaugeSlider.value += gaugeIncreaseAmount;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
